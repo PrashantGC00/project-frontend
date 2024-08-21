58a8834cc53d81dc73cb48a9d0fdfcf2f0190aab
Prashant G.C <106686813+PrashantGC00@users.noreply.github.com>
2024-08-21 22:36:28 +0545

    Transitions

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -13,27 +13,26 @@ const MainNavigation = props =>{
 
     const [drawerIsOpen, setDrawerIsOpen] = useState(false)
 
-    const openDrawer = () =>{
+    const openDrawerHandler = () =>{
         setDrawerIsOpen(true)
     }
 
-    const closeDrawer = () =>{
+    const closeDrawerHandler = () =>{
         setDrawerIsOpen(false)
     }
 
     return ( 
         <React.Fragment>
-            {drawerIsOpen ? <Backdrop onClick={closeDrawer} /> : null}
-            {drawerIsOpen ? 
-            (
-        <SideDraw>
+            {drawerIsOpen ? <Backdrop onClick={closeDrawerHandler} /> : null}
+            
+        <SideDraw show={drawerIsOpen} onClick={closeDrawerHandler}>
             <nav className="main-navigation__drawer-nav">
                 <NavLinks />
             </nav>
         </SideDraw> ) : null
-        }
+        
         <MainHeader>
-                <button className="main-navigation__menu-btn" onClick={openDrawer}>
+                <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
                     <span />
                     <span />
                     <span />
