e7f8e96a07d5c216830c3a774c50f689256f111e
Prashant G.C <106686813+PrashantGC00@users.noreply.github.com>
2024-09-02 12:17:49 +0545

    React validator prototype

--- src/places/pages/Place.js
+++ src/places/pages/Place.js
@@ -7,7 +7,7 @@ import Input from "../../shared/components/FormElements/Input";
 const Place = () => {
     return (
         <form className="place-form">
-            <Input element='input' type='text' label='Title'/>
+            <Input element='input' type='text' label='Title' validators={[]} onInput={[]} errorText="Please Enter A Valid Title" />
         </form>    
     )
     
